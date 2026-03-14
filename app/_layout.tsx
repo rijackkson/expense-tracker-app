66d075f9bc2000343d89ecde1cd7ded109397759
rijackkson <rijackkson@gmail.com>
2026-03-14 11:24:15 -0300

    first commit

---
+++ app/_layout.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet } from 'react-native'
+
+const _layout = () => {
+  return (
+    <Stack screenOptions={{ headerShown: false }}></Stack>
+  )
+}
+
+export default _layout
+
+const styles = StyleSheet.create({})
